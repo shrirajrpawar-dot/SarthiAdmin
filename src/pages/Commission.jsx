1ea9c9044f47a3bd73351dd2866863576a83d10a
Shriraj Pawar <your-email@gmail.com>
2026-05-14 23:59:06 +0530

    Commissionfixlast

--- src/pages/Commission.jsx
+++ src/pages/Commission.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { collection, query, where, onSnapshot, orderBy, updateDoc, doc, getDocs, writeBatch } from 'firebase/firestore';
 import { db } from '../firebase';
-import Card from '../components/Card';
+import { Card } from '../components/Card';
 
 const tokens = {
   bgPanel: '#F3F4F6',
